69585231d05958134973b540a3de0a3e796bfcee
domster704 <domster704@mail.ru>
2022-01-15 21:30:18 +0400

    just

--- server.py
+++ server.py
@@ -1,37 +1,30 @@
 import cv2
 import jsonpickle
 import numpy as np
-from flask import Flask, request, Response, jsonify
+from flask import Flask, request, Response
 from Detector import Detector
 
 app = Flask(__name__)
-detector = None
 
 
 @app.route('/api/detectBear', methods=['POST'])
 def detect():
-	global detector
 	nparr = np.frombuffer(request.data, np.uint8)
 	img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
 	detector = Detector(img)
 	detector.doDetect()
 
-	newImg = detector.getImgAfterDetection()
+	if detector.isBearExisted():
+		newImg = detector.getImgAfterDetection()
 
-	_, img_encoded = cv2.imencode('.jpg', newImg)
+		_, img_encoded = cv2.imencode('.jpg', newImg)
 
-	response_pickled = jsonpickle.decode(jsonpickle.encode(img_encoded)).tobytes()
+		response_pickled = jsonpickle.decode(jsonpickle.encode(img_encoded)).tobytes()
 
-	return Response(response=response_pickled, status=200, mimetype=None)
-
-
-@app.route('/api/getStatus', methods=['GET'])
-def getStatus():
-	if detector is not None and isinstance(detector, Detector):
-		return jsonify(detector.isBearExisted())
+		return Response(response=response_pickled, status=200)
 	else:
-		return jsonify("Вы ещё не просканировали фото")
+		return Response(status=204)
 
 
 @app.route("/", methods=['GET'])
