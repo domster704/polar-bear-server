124889224dee1edf82355a78014dd3ccbf79723a
domster704 <domster704@mail.ru>
2022-01-15 17:29:31 +0400

    server

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, Response
+import jsonpickle
+import numpy as np
+import cv2
+
+app = Flask(__name__)
+
+
+@app.route('/api/test', methods=['POST'])
+def test():
+	r = request
+	nparr = np.frombuffer(r.data, np.uint8)
+	img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+	_, img_encoded = cv2.imencode('.jpg', img)
+	# response = {'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0])
+	# 			}
+	# response_pickled = jsonpickle.encode(response)
+
+	response_pickled = jsonpickle.encode(img_encoded)
+	# print(jsonpickle.decode(response_pickled).tobytes())
+	response_pickled = jsonpickle.decode(response_pickled).tobytes()
+
+	return Response(response=response_pickled, status=200, mimetype="bytes")
+
+
+app.run(host="0.0.0.0", port=5000)
